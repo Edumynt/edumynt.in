3243fc52eb45c656148ba663c453dcc08b8a7207
iamkdraj <iamkdraj@gmail.com>
2025-07-30 03:12:03 +0530

    bwtheme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    tailwind({
+      applyBaseStyles: false,
+    })
+  ],
   markdown: {
     shikiConfig: {
-      theme: 'github-dark-dimmed',
+      theme: 'github-dark',
       wrap: true,
     },
   },
