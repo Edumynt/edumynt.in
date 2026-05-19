93b91cb7d8a19ca3ae7977be09a5b1ff7b7b8ea3
iamkdraj <iamkdraj@gmail.com>
2026-05-19 04:39:15 +0000

    Move Expo app outside Astro repo

--- edumyntinapp/lib/nativewind.ts
+++
@@ -1,6 +0,0 @@
-import { LinearGradient } from 'expo-linear-gradient';
-import { cssInterop } from 'nativewind';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-cssInterop(SafeAreaView, { className: 'style' });
-cssInterop(LinearGradient, { className: 'style' });
